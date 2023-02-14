4d50dee23b919bdba05cf53aa5317617169fd4f5
MrALAMIN156 <115931755+MrALAMIN156@users.noreply.github.com>
2023-02-14 23:42:45 +0600

    Update ALAMIN.py

--- ALAMIN.py
+++ ALAMIN.py
@@ -1,1 +1,56 @@
 
+import os, platform
+
+ 
+
+try:
+
+ 
+
+        import requests
+
+ 
+
+except:
+
+ 
+
+        os.system('pip2 install requests')
+
+ 
+
+ 
+
+ 
+
+bit = platform.architecture()[0]
+
+ 
+
+if bit == "64bit":
+
+        os.system('xdg-open https://youtube.com/@teach-alamin')
+
+ 
+
+        from file9 import alaminbro
+
+ 
+
+        ALAMIN_Main()
+
+ 
+
+ 
+
+ 
+
+elif bit == "32bit":
+
+ 
+
+        os.system('xdg-open https://youtube.com/@teach-alamin')
+
+ 
+
+        os.system('python file9.py')
